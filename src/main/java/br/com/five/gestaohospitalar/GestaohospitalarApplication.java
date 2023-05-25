0729ff6dba56a3d3a5d82c7d7bb804068bef1c4f
Yuri Nascimento <yuriidiiego@gmail.com>
2023-05-24 22:01:17 -0300

    chore: 🎉 Inital commit

---
+++ src/main/java/br/com/five/gestaohospitalar/GestaohospitalarApplication.java
@@ -0,0 +1,12 @@
+package br.com.five.gestaohospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaohospitalarApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GestaohospitalarApplication.class, args);
+  }
+}
---
+++ src/test/java/br/com/five/gestaohospitalar/GestaohospitalarApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.five.gestaohospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaohospitalarApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
